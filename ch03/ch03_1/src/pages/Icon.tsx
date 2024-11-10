8e749c468b6ce60de4f78d724f7f3345a7d6d23d
안정하 <anjeongha@anjeonghaui-MacBookPro.local>
2024-11-10 12:51:35 +0900

    css

--- ch03/ch03_1/src/pages/Icon.tsx
+++ ch03/ch03_1/src/pages/Icon.tsx
@@ -1,9 +1,9 @@
 export default function Icon() {
   return (
-  <div>
-  <h3>Icon</h3>
-  <span className="material-icons">home</span>
-  <span className="material-icons">check_circle_outline</span>
-  </div>
+    <div>
+      <h3>Icon</h3>
+      <span className="material-icons">home</span>
+      <span className="material-icons">check_circle_outline</span>
+    </div>
   )
-  }
+}

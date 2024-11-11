9d797f8343e81675c4be46b9a9e25075a7ab5ce6
안정하 <anjeongha@anjeonghaui-MacBookPro.local>
2024-11-11 23:23:56 +0900

    lean tailwindcss

---
+++ ch03/ch03_2/src/App.tsx
@@ -0,0 +1,12 @@
+import Tailwindcss from './pages/Tailwindcss'
+import Color from './pages/Color'
+import TextsTest from './pages/TextTest'
+export default function App() {
+  return (
+    <div>
+      <TextsTest />
+      <Color />
+      <Tailwindcss />
+    </div>
+  )
+}
---
+++ ch03/ch03_2/src/data/index.ts
@@ -0,0 +1,3 @@
+export const randomParagraphs = (length: number): string => {
+  return '임시 텍스트'.repeat(length)
+}

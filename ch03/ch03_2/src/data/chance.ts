63c8ef2c89a9fc5047907e17c4cf2810b0594ea0
ajh <jhan@ntro.kr>
2024-12-03 15:49:32 +0900

    css 상자

--- ch03/ch03_2/src/data/chance.ts
+++ ch03/ch03_2/src/data/chance.ts
@@ -9,3 +9,4 @@ export const randomCompanyName = () => chance.company()
 export const randomSentence = (words = 5) => chance.sentence({words})
 export const randomTitleText = (words = 3) => chance.sentence({words})
 export const randomParagraphs = (sentences = 3) => chance.paragraph({sentences})
+

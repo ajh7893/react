bcd7fb39d5378fdfbcfca6a26c861d73c9d13460
ajh <jhan@ntro.kr>
2024-12-04 15:56:46 +0900

    3_4 second

--- ch03/ch03_4/src/components/Div.tsx
+++ ch03/ch03_4/src/components/Div.tsx
@@ -6,38 +6,34 @@ import type {
 } from "react";
 import type { WidthHeight } from "./WidthHeight";
 import type { LeftRightTopBottom } from "./LeftRightTopBottom";
-
+import type { MinMaxWidthHeight } from "./MinMaxWidthHeight";
 export type ReactDivProps = DetailedHTMLProps<
   HTMLAttributes<HTMLDivElement>,
   HTMLDivElement
 >;
 export type DivProps = ReactDivProps &
   PropsWithChildren<WidthHeight> &
-  LeftRightTopBottom & {
+  LeftRightTopBottom &
+  MinMaxWidthHeight & {
     src?: string;
   };
+// prettier-ignore
 export const Div: FC<DivProps> = ({
-  width,
-  height,
-  style: _style,
-  src,
-  className: _className,
-  left,
-  right,
-  top,
-  bottom,
-  ...props
+width, height,
+style: _style,
+src,
+className: _className,
+left, right, top, bottom,
+minWidth, maxWidth, minHeight, maxHeight,
+...props
 }) => {
-  const style = {
-    ..._style,
-    width,
-    height,
-    backgroundImage: src && `url(${src})`,
-    left,
-    right,
-    top,
-    bottom,
-  };
-  const className = ["box-sizing", src && "bg-gray-300", _className].join(" ");
-  return <div {...props} className={className} style={style} />;
-};
+const style = {
+..._style,
+width, height,
+backgroundImage: src && `url(${src})`,
+left, right, top, bottom,
+minWidth, maxWidth, minHeight, maxHeight
+}
+const className = ['box-sizing', src && 'bg-gray-300', _className].join(' ')
+return <div {...props} className={className} style={style} />
+}
--- ch03/ch03_4/src/pages/MinMaxTest.tsx
+++ ch03/ch03_4/src/pages/MinMaxTest.tsx
@@ -1,9 +1,19 @@
-import { Title } from "../components";
-export default function CopyMe() {
+import { Div, Title } from "../components";
+import * as D from "../data";
+export default function MinMaxTest() {
+  // prettier-ignore
   return (
     <section className="mt-4">
-      <Title>CopyMe</Title>
-      <div className="mt-4"></div>
+      <Title>MinMaxTest</Title>
+      <Div className="p-4 bg-gray-300">
+        <Div src={D.randomImage(800, 300)} className="bg-cover">
+          <Div
+            className="w-1/2 h-80 bg-blue-500"
+            minWidth="300px"
+            maxWidth="500px"
+          />
+        </Div>
+      </Div>
     </section>
   );
 }

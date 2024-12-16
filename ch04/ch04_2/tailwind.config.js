4d265f92350ccf8f12edad36d11b0d4068fb2580
ajh <jhan@ntro.kr>
2024-12-16 15:02:03 +0900

    ch04_2-hook-source-update

--- ch04/ch04_2/tailwind.config.js
+++ ch04/ch04_2/tailwind.config.js
@@ -1,11 +1,9 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html", // 루트에 있는 HTML 파일
-    "./src/**/*.{js,ts,jsx,tsx}", // 소스 코드에 포함된 모든 JS/TS/JSX/TSX 파일
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
-};
+plugins: [require('@tailwindcss/line-clamp'), require('daisyui')],
+}

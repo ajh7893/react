2518ae9e36fcca3bafd03d15ed1627595f8f0339
ajh <jhan@ntro.kr>
2025-01-17 16:09:36 +0900

    fetch() 함수와 Promise

--- ch04/ch04_4/src/pages/FetchTest.tsx
+++ ch04/ch04_4/src/pages/FetchTest.tsx
@@ -1,9 +1,60 @@
-import { Title } from "../components";
-export default function CopyMe() {
+import { useState, useCallback, useEffect } from "react";
+import { useToggle } from "../hooks";
+import { Title, Avatar, Icon } from "../components";
+import * as D from "../data";
+
+export default function FetchTest() {
+  const [loading, toggleLoading] = useToggle();
+  const [randomUser, setRandomUser] = useState<D.IRandomUser | null>(null);
+  const [error, setError] = useState<Error | null>(null);
+
+  const getRandomUser = useCallback(async () => {
+    try {
+      toggleLoading();
+      const user = await D.fetchRandomUser();
+      setRandomUser(user);
+    } catch (error) {
+      setError(error as Error);
+    } finally {
+      toggleLoading();
+    }
+  }, [toggleLoading]);
+
+  useEffect(() => {
+    getRandomUser();
+  }, [getRandomUser]);
+
   return (
     <section className="mt-4">
-      <Title>CopyMe</Title>
-      <div className="mt-4"></div>
+      <Title>FetchTest</Title>
+      <div className="flex justify-center mt-4">
+        <button className="btn btn-sm btn-primary" onClick={getRandomUser}>
+          <Icon name="get_app" />
+          <span>Get Random User</span>
+        </button>
+      </div>
+      {loading && (
+        <div className="flex items-center justify-center">
+          <button className="btn btn-circle loading"></button>
+        </div>
+      )}
+      {error && (
+        <div className="p-4 mt-4 bg-red-200">
+          <p className="text-3xl text-red-500 font-bold">{error.message}</p>
+        </div>
+      )}
+      {randomUser && (
+        <div className="flex justify-center p-4 mt-4">
+          <Avatar src={randomUser.picture.large} />
+          <div className="ml-4">
+            <p className="text-xl font-bold">
+              {randomUser.name.title}. {randomUser.name.first}{" "}
+              {randomUser.name.last}
+            </p>
+            <p className="italic text-gray-600">{randomUser.email}</p>
+          </div>
+        </div>
+      )}
     </section>
   );
 }

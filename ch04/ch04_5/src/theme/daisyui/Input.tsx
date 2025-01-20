5d9410afcbb3a9ec4575ff0fdf941488da578aed
ajh <jhan@ntro.kr>
2025-01-20 15:54:11 +0900

    ValidatableInput 컴포넌트 만들기

--- ch04/ch04_5/src/theme/daisyui/Input.tsx
+++ ch04/ch04_5/src/theme/daisyui/Input.tsx
@@ -1,13 +1,12 @@
-import type {FC, DetailedHTMLProps, InputHTMLAttributes} from 'react'
-
+import type { DetailedHTMLProps, InputHTMLAttributes } from "react";
+import { forwardRef } from "react";
+// prettier-ignore
 export type ReactInputProps = DetailedHTMLProps<
-  InputHTMLAttributes<HTMLInputElement>,
-  HTMLInputElement
->
-
-export type inputProps = ReactInputProps & {}
-
-export const Input: FC<inputProps> = ({className: _className, ...inputProps}) => {
-  const className = ['input', _className].join(' ')
-  return <input {...inputProps} className={className} />
-}
+InputHTMLAttributes<HTMLInputElement>,
+HTMLInputElement>
+export type InputProps = ReactInputProps & {};
+export const Input = forwardRef<HTMLInputElement, InputProps>((props, ref) => {
+  const { className: _className, ...inputProps } = props;
+  const className = ["input", _className].join(" ");
+  return <input ref={ref} {...inputProps} className={className} />;
+});

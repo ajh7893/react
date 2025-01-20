473d67dc697c8a01d188bacb81dcfd555cbc1e41
ajh <jhan@ntro.kr>
2025-01-20 15:25:49 +0900

    focus 사용

--- ch04/ch04_5/src/pages/InputFocusTest.tsx
+++ ch04/ch04_5/src/pages/InputFocusTest.tsx
@@ -1,9 +1,29 @@
+import { useRef, useEffect } from "react";
 import { Title } from "../components";
-export default function CopyMe() {
+
+export default function InputFocusTest() {
+  const inputRef = useRef<HTMLInputElement>(null);
+  useEffect(() => inputRef.current?.focus(), []);
+  {
+    /*포커스*/
+  }
+
   return (
     <section className="mt-4">
-      <Title>CopyMe</Title>
-      <div className="mt-4"></div>
+      <Title>InputFocusTest</Title>
+      <div className="flex justify-center mt-4 ">
+        <input
+          ref={inputRef}
+          className="input input-primary"
+          placeholder="enter some text"
+        />
+        <input
+          ref={inputRef}
+          className="input input-primary"
+          placeholder="enter some text"
+        />{" "}
+        {/* 두개의 포커스라면 마지막에 포커스가 적용된다.,  */}
+      </div>
     </section>
   );
 }
